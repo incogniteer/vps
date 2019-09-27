5c32b11f9531535cb59ccf82badccaccebb1a95f
Leonhard Euler <34206438+incogniteer@users.noreply.github.com>
2019-09-27 23:33:17 +0800

    Update vps.sh

--- vps.sh
+++ vps.sh
@@ -20,6 +20,26 @@ alias cls="echo -en '\\x1bc'"
 alias vi=vim
 alias sudo='sudo ' #Enable alias in sudo
 
+alias rmdir='rmdir -pv'
+alias path='echo -e ${PATH//:/\\n}'
+alias grep='grep --color=auto'
+alias egrep='grep -E --color=auto'
+alias fgrep='grep -F --color=auto'
+alias less='less -RN'
+alias sys='systemctl'
+alias syss='sys status -l'
+alias now='date +%T'
+alias nowd='date +%F'
+
+alias ..='cd ../'
+alias ...='cd ../../'
+alias ....='cd ../../../'
+alias .....='cd ../../../../'
+alias .3='cd ../../../'
+alias .4='cd ../../../../'
+alias .5='cd ../../../../../'
+alias r='fc -s'
+
 [[ $- == *i* ]] && stty -ixon
 # stty command is executed only if a tty is attached to the process.
 # stty istrip : Strip input characters to 7 bits
