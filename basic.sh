0262708599a6068c346b24c48e9f47ba2796e3aa
Leonhard Euler <34206438+incogniteer@users.noreply.github.com>
2019-09-28 22:41:59 +0800

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -50,31 +50,4 @@ export EDITOR="$(command -pv vim)"
 
 EOF
 
-cat >>/etc/vimrc <<EOF
-
-" User-defined configs
-
-" General
-syntax on
-colorscheme desert
-filetype plugin indent on
-set nocompatible
-
-" Tab 
-set tabstop=4
-set shiftwidth=4
-set softtabstop=4
-set expandtab
-
-" Indentation
-set cindent
-set autoindent
-set smartindent
-
-" Misc
-set number
-set hlsearch
-
-EOF
-
 fi
