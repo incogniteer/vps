9e55dd9231cddee4cedd4edf0feb269e061892d1
Leonhard Euler <34206438+incogniteer@users.noreply.github.com>
2019-09-28 23:51:12 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+# Install basic packages
+yum -y install wget git epel-release screen
+yum install -y pcre pcre-devel git gettext gcc autoconf libtool automake make asciidoc xmlto c-ares-devel libev-devel
