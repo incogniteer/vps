ea4978466313cee80b5fe93aeddc25255fa75b5f
Leonhard Euler <incogniteer@gmail.com>
2019-10-18 22:28:27 +0800

    add then;

--- upgrade_kernel.sh
+++ upgrade_kernel.sh
@@ -2,7 +2,7 @@
 
 yum -y update
 
-if [ $(sort -t. -k1,1 2,2 <(echo 4.90) <(uname -r) -gr | head -n1) = 4.90 ]; then
+if [ $(sort -t. -k1,1 -k2,2 <(echo 4.90) <(uname -r) -gr | head -n1) = 4.90 ]; then
     rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
     rpm -Uvh  https://www.elrepo.org/elrepo-release-7.0-4.el7.elrepo.noarch.rpm
     yum --disablerepo="*" --enablerepo="elrepo-kernel" -y install kernel-ml
