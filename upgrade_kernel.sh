8cd061803d56297d69e169602892e0e0431bb077
Leonhard Euler <incogniteer@gmail.com>
2019-10-18 22:19:18 +0800

    script for upgrading kernel

---
+++ upgrade_kernel.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+yum -y update
+
+if [ $(sort -t. -k1,1 2,2 <(echo 4.90) <(uname -r) -gr | head -n1) = 4.90 ] then
+    rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+    rpm -Uvh  https://www.elrepo.org/elrepo-release-7.0-4.el7.elrepo.noarch.rpm
+    yum --disablerepo="*" --enablerepo="elrepo-kernel" -y install kernel-ml
+    grub2-set-default 0
+    grub2-mkconfig -o /boot/grub2/grub.cfg
+    reboot
+fi
