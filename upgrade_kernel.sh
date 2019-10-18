f84a23749d5bd3865534e835f5163e753a843413
Leonhard Euler <34206438+incogniteer@users.noreply.github.com>
2019-10-18 22:47:07 +0800

    Update upgrade_kernel.sh

--- upgrade_kernel.sh
+++ upgrade_kernel.sh
@@ -7,6 +7,6 @@ if [ $(sort -t. -k1,1 -k2,2 <(echo 4.90) <(uname -r) -gr | head -n1) = 4.90 ]; t
     rpm -Uvh  https://www.elrepo.org/elrepo-release-7.0-4.el7.elrepo.noarch.rpm
     yum --disablerepo="*" --enablerepo="elrepo-kernel" -y install kernel-ml
     grub2-set-default 0
-    grub2-mkconfig -o /boot/grub2/grub.cfg
+    grub2-mkconfig -o /boot/grub2/grub.cfg &&
     reboot
 fi
