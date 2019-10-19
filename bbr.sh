bf0ca1eeabe9d9521a0f87546a14390369452f5f
Leonhard Euler <incogniteer@gmail.com>
2019-10-19 16:18:20 +0800

    sysconf.d -> sysctl.d

--- bbr.sh
+++ bbr.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-CUSTOM_BBR_CONF=/etc/sysconf.d/bbr.conf
+CUSTOM_BBR_CONF=/etc/sysctl.d/bbr.conf
 
 if ! [ sysctl -n net.ipv4.tcp_available_congestion_control | grep -i bbr &>/dev/null\
      || lsmod | grep -i bbr &>/dev/null ]; then
