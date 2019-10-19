00f820a98a88fed49fe5c2021f16d608c236ee8e
Leonhard Euler <incogniteer@gmail.com>
2019-10-19 16:24:07 +0800

    none

--- bbr.sh
+++ bbr.sh
@@ -2,8 +2,9 @@
 
 CUSTOM_BBR_CONF=/etc/sysctl.d/bbr.conf
 
-if ! [ sysctl -n net.ipv4.tcp_available_congestion_control | grep -i bbr &>/dev/null\
-     || lsmod | grep -i bbr &>/dev/null ]; then
+if ! [ sysctl -n net.ipv4.tcp_available_congestion_control -q | \
+       grep -i bbr &>/dev/null ||
+       lsmod | grep -i bbr -qs ]; then
 
 echo "net.core.default_qdisc=fq" | tee -a $CUSTOM_BBR_CONF
 echo "net.ipv4.tcp_congestion_control=bbr" | tee -a $CUSTOM_BBR_CONF
