1d82ce73ad28b857d94be14f7c999bcda6538e50
Leonhard Euler <34206438+incogniteer@users.noreply.github.com>
2019-11-11 23:31:03 +0800

    Update shadowsocks-local-install.sh

--- shadowsocks-local-install.sh
+++ shadowsocks-local-install.sh
@@ -64,10 +64,9 @@ chacha20-ietf-poly1305
 xchacha20-ietf-poly1305
 )
 
-select cipher in $ciphers;
+select cipher in "${ciphers[@]};
 do
-    case $cipher in
-      aes-256-gcm|aes-256-cfb|chacha20-ietf-poly1305|xchacha20-ietf-poly1305)
+    case $cipher in aes-256-gcm|aes-256-cfb|chacha20-ietf-poly1305|xchacha20-ietf-poly1305)
       echo "You selected $cipher!"
       cipher=$cipher
       break
