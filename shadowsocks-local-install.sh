b72d83d489147373fd34e240c3c191c919952601
Leonhard Euler <34206438+incogniteer@users.noreply.github.com>
2019-11-11 23:33:37 +0800

    Update shadowsocks-local-install.sh

--- shadowsocks-local-install.sh
+++ shadowsocks-local-install.sh
@@ -42,19 +42,20 @@ fi
 #Configurations
 #Get port number
 read -p "Please set up a server port(Default: 18388): " server_port
-echo "You have selected server port: $server_port."
-read -p "Please set up server address: " server_address
-echo "You have selected server address: $server_address."
 
 #Check if port valid
 #POSIX ERE not supporting \d or \w. Using [[:digit:]] [0-9] and ^$ instead
 #" ; " is required before then
 #Using -gt -lt for arithmetic. > < for strings!
 #Using while or for loop, instead of if, then construct
-#while [[ ! ( $server_port =~ ^[[:digit:]]{4,5}$ && $server_port -gt 1024 && $server_port -lt 65535 ) ]]; do
-#  echo -n "Please enter port number between 1024 and 65535: "
-#  read server_port
+while [[ ! ( $server_port =~ ^[[:digit:]]{4,5}$ && $server_port -gt 1024 && $server_port -lt 65535 ) ]]; do
+  echo -n "Please enter port number between 1024 and 65535: "
+  read server_port
 #done
+
+echo "You have selected server port: $server_port."
+read -p "Please set up server address: " server_address
+echo "You have selected server address: $server_address."
 
 #Get ciper method
 ciphers=(
