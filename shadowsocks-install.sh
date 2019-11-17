b7f2b949a64be469f3ba7b3f8100697514f944ca
Leonhard Euler <incogniteer@gmail.com>
2019-11-17 18:13:21 +0800

    misc

--- shadowsocks-install.sh
+++ shadowsocks-install.sh
@@ -134,8 +134,6 @@ cat > config.json <<eof
 {
   "server":"0.0.0.0",
   "server_port":"$server_port",
-  "local_address":"127.0.0.1",
-  "local_port":1080,
   "password":"884595ds12",
   "timeout":60,
   "method":"$cipher",
