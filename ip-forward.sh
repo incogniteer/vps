c19628c847827f940274d95f62ccbd8d9afc0538
Leonhard Euler <incogniteer@gmail.com>
2019-11-19 23:03:53 +0800

    miscellaneous

--- ip-forward.sh
+++ ip-forward.sh
@@ -1,29 +1,63 @@
 #!/usr/bin/env bash
 
+set -o errexit
+set -o nounset
+set -o pipefaile
+
+RED='\033[0;31m'
+NC='\033[0m'
+
+DEBUG=false
+[[ "$DEBUG" == *true* ]] && set -o xtrace
+
+err() {
+    printf "${RED}%s${NC}\n" "Something is wrong, exiting..."
+}
+
+cleanup() {
+    printf "${RED}%s${NC}\n" "Something is wrong, exiting..."
+}
+
+BWH=216.24.183.179
+HOSTDARE=185.238.250.78 
+VIRMACH=122.51.173.88
+TXHK=124.156.100.46
+ALIHK=47.240.38.80
+
 #Forward shadowsocks traffic through an internal vps in CN
 #Enable ipv4 forward
 echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.d/enable_ip_forward && sysctl -p /etc/sysctl.d/*
 
 #Enable firewalld
-systemctl -q is-enabled firewalld >/dev/null 2>&1
-! [ $? -eq 0 ] && systemctl -q enable --now firewalld
+! systemctl -q is-enabled firewalld >/dev/null 2>&1 &&
+systemctl -q enable --now firewalld
+
+#Enable masquerading
+firewall-cmd -q --permanent --add-masquerade && firewall-cmd -q --reload
 
 #Allow TCP&UDP ports
 #Note: brace expansion can be used!
 #Note: source forwarded ports MUST be different!
-firewall-cmd -q --permanent --add-port={18888,18388}/{tcp,udp}
 
 #USLA BWH
-firewall-cmd -q --permanent --add-forward-port=port=18388:proto={tcp,udp}:toport=18388:toaddr=216.24.183.179 && firewall-cmd -q --reload
+enable_forward() {
+    local IP=$1
+    local FROM_PORT=$2
+    local TO_PORT=$3
 
-#USSA VIRMACH
-firewall-cmd --permanent --add-forward-port=port=18188:proto={tcp,udp}:toport=18388:toaddr=107.174.240.124 && firewall-cmd -q --reload
+    if firewall-cmd --list-ports | tr -d '(/udp|/tcp)' | grep -w $FROM_PORT; then
+        :
+    else
+    firewall-cmd -q --permanent --zone=public --add-port=$FROM_PORT/{tcp,udp}
+    fi
 
-#USLA HOSTDARE
-firewall-cmd -q --permanent --add-forward-port=port=18588:proto={tcp,udp}:toport=18388:toaddr=185.238.250.78 && firewall-cmd -q --reload
+    firewall-cmd -q --permanent --zoone=public \
+--add-forward-port=port=${FROM_PORT}:proto={tcp,udp}:toport=${TO_PORT}:toaddr=${IP} && 
+    firewall-cmd -q --reload
+}
 
-#HK-TECENT 
-firewall-cmd -q --permanent --add-forward-port=port=18888:proto={tcp,udp}:toport=18888:toaddr=124.156.100.46 && firewall-cmd -q --reload
-
-#Enable masquerading
-firewall-cmd -q --permanent --add-masquerade && firewall-cmd -q --reload
+enable_forward $BWH 18388 18388
+enable_forward $VIRMACH 18188 18388
+enable_forward $HOSTDARE 18588 38888
+enable_forward $TXHK 18888 18888
+enable_forward $ALIHK 18688 18388
