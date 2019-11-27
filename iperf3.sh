fbcba1fc8a1a72d3db3b7c8bad27da34a185f3c5
Leonhard Euler <incogniteer@gmail.com>
2019-11-27 20:22:39 +0800

    miscellaneous

--- iperf3.sh
+++ iperf3.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-[ rpm -qa | grep iperf3 ] || yum -y install iperf3 && \
+#[ rpm -qa | grep iperf3 ], wrong usage of [ just rpm -qa | grep iperf3 
+rpm --quiet -q iperf3  || yum -y install iperf3 && \
     firewall-cmd --zone=public --permanent --add-port=5201/{tcp,udp} \
     && firewall-cmd --reload
