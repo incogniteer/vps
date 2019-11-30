16ed4cfed9f2d72c7b7c1e2d4b5d66ec954a9fc9
Leonhard Euler <34206438+incogniteer@users.noreply.github.com>
2019-11-30 18:20:19 +0800

    Update kernel-upgrade.sh

--- kernel-upgrade.sh
+++ kernel-upgrade.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-yum -y update
+#rpm -q --queryformat '%{VERSION}' centos-release
+os_ver=$(rpm -q --queryformat '%{VERSION}' centos-release)
+
+if [[ ${os_ver} =~ 7 ]]; then
+yum -y update && yum -y upgrade
 
 if [ $(sort -t. -k1,1 -k2,2 <(echo 4.90) <(uname -r) -gr | head -n1) = 4.90 ]; then
     rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
@@ -9,4 +13,19 @@ if [ $(sort -t. -k1,1 -k2,2 <(echo 4.90) <(uname -r) -gr | head -n1) = 4.90 ]; t
     grub2-set-default 0
     grub2-mkconfig -o /boot/grub2/grub.cfg &&
     reboot
+fi
+
+else
+
+dnf -y update && dnf -y upgrade
+
+if [ $(sort -t. -k1,1 -k2,2 <(echo 4.90) <(uname -r) -gr | head -n1) = 4.90 ]; then
+    rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+    rpm -Uvh https://www.elrepo.org/elrepo-release-7.0-4.el7.elrepo.noarch.rpm
+    yum --disablerepo="*" --enablerepo="elrepo-kernel" -y install kernel-ml
+    grub2-set-default 0
+    grub2-mkconfig -o /boot/grub2/grub.cfg &&
+    reboot
+fi
+
 fi
