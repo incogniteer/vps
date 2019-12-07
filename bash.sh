2c26f3464bec07897e22b1d8098648465baaf735
incogniteer <incogniteer@gmail.com>
2019-12-08 01:08:02 +0800

    miscellaneous

--- bash.sh
+++ bash.sh
@@ -3,6 +3,7 @@
 set -o errexit
 set -o nounset
 set -o pipefail
+set -o histexpand
 
 finish() {
     printf "%s\n" "Something went wrong, exiting..."
@@ -10,36 +11,42 @@ finish() {
 
 trap finish ERR
 
-pkgs=(wget screen vim epel-release)
+#Set time zone corresponding to /usr/share/zoneinfo format
+TIMEZONE="Asia/Shanghai"
+pkgs=(wget 
+      screen 
+      vim 
+      epel-release
+)
+
 for pkg in "${pkgs[@]}"; do
-    rpm -q "${pkg}" || yum -y install "${pkg}"
+    { rpm -q "${pkg}" || which "${pkg}" &>/dev/null; }  || yum -y install "${pkg}"
 done
 
 #Alternatively: timedatectl set-timezone Asia/Shanghai
-ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+ln -sf /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
 
 #Add [ ] otherwise will report errors
-cat >> /etc/bashrc <<EOF
-
-#User defined bash configs
-alias ls='ls -h --color=auto'
+cat > /etc/profile.d/alias.sh <<EOF
+#Common aliases
+alias ls='ls --color=auto'
+alias l.='ls -d .*'
 alias l='ls -CF'
-alias la='ls -al'
-alias ld='ls -dl'
+alias ll='ls -Fhl'
+alias la='ls -ahFl'
+alias lA='ls -AhFl'
+alias ld='ls -dhFl'
 alias l1='ls -1'
-alias lt='l -t'
+alias lt='ll -t --time-style=+"%F %T"'
 alias lr='lt -r'
 alias li='ls -i'
 alias rm='rm -v'
 alias mv='mv -v'
 alias cp='cp -v'
-alias rmdir='rmdir -v'
-alias mkdir='mkdir -pv'
-alias cls="echo -en '\\x1bc'"
+alias cls='echo -en \\x1bc'
 alias vi=vim
-alias py='python3'
+alias py='py ' #Note the trailing space
 alias sudo='sudo ' #Enable alias in sudo
-alias source='. ~/.bashrc'
 
 alias rmdir='rmdir -pv'
 alias path='echo -e ${PATH//:/\\n}'
@@ -51,9 +58,8 @@ alias sys='systemctl'
 alias syss='sys status -l'
 alias now='date +%T'
 alias nowd='date +%F'
-alias nowdate=nowd
 
-alias cd..='cd ..'
+alias cd..='cd ../'
 alias ..='cd ../'
 alias ...='cd ../../'
 alias ....='cd ../../../'
@@ -63,20 +69,75 @@ alias .4='cd ../../../../'
 alias .5='cd ../../../../../'
 alias r='fc -s'
 alias ret='echo $?'
+alias path='echo -e ${PATH//:/\\n}'
 alias type='type -a'
+alias r='fc -s' #Use fc alias fc -s pat=rep cmd
+EOF
 
-#Alternatively, [ -t 0 ] or if tty -s >/dev/null 2>&1; then
-#[ -t 0 ] && stty -ixon
-#tty -s >/dev/null 2>&1 && stty -ixon
-# Turn on "Ctrl-s" for forward history search
+cat >/etc/profile.d/func.sh <<EOF
+#Bash functions definitions
+# kill bash sessions
+killbash () {
+    local pids=( $(pgrep bash | grep -v $$) )
+    for pid in "${pids[@]}"; do kill -9 $pid; done
+} 2>/dev/null
+
+# hisotry entries
+h() {
+    #Positional parameter and special can't assign in ${var:=} use if [ -n $var ]
+    #Or ${var:-}
+    local num=${1:-15}
+    if [[ ! $num =~ [[:digit:]]+ ]]; then
+        echo "Usage: h n(numeric argument!)..."
+    else
+    history $num
+fi
+}
+
+#Aliase for one word, func preferred over alias, esp. for +2 words
+git() {
+    command git commit -a -m "miscellaneous" &&
+    command git push
+}
+
+
+#below is incorret will fall into resurive call
+#mkdir() {
+#    mkdir -p $1
+#    cd $1
+#}
+
+mkdir() {
+    command mkdir -p $1 &&
+    cd -P $1
+}
+
+#Or
+mkcd() {
+    command mkdir -p $1 &&
+    cd -P $1
+    #work in command linecd -P $1
+}
+EOF
+
+
+cat >/etc/profile.d/misc.sh <<EOF
+# history related items
+shopt -s histappend
+HISTFILE=~/.bash_history
+HISTIGNORE=cls:h
+HISTCONTROL=ignoredups:erasedupsi:ignorespace
+HISTTIMEFORMAT='%F %T '
+HISTSIZE=
+HISTFILESIZE=
+PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a;history -c;history -r"
+
 # stty command is executed only if a tty is attached to the process.
-[[ $- == *i* ]] && stty -ixon
 # stty istrip : Strip input characters to 7 bits
 tty -s && stty istrip
 
+[[ $- == *i* ]] && stty -ixon
+
 export VISUAL="$(command -pv vim)"
 export EDITOR="$(command -pv vim)"
-
 EOF
-
-[ $? -eq 0 ] && . /etc/bashrc
